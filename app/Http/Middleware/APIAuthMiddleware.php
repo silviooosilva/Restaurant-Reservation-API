1725e81a86ebda7160ff34f7462bd41a266c02b6
Sílvio Silva <gasparsilvio7@gmail.com>
2024-06-29 22:02:32 +0100

    Guard Auth API in Middleware

--- app/Http/Middleware/APIAuthMiddleware.php
+++ app/Http/Middleware/APIAuthMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Utils\Helper;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,11 +17,15 @@ class APIAuthMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(!$request->has('email') && !$request->has('password')){
-            if(Auth::guard('api')->guest() || !Auth::guard('api')->check()){
-                return response()->json(['error' => 'Unauthorized'], 401);
+        if (!$request->has('email') && !$request->has('password')) {
+            if (Auth::guard('api')->guest() || !Auth::guard('api')->check()) {
+                Helper::ResponseAPI(['error' => 'Unauthorized'], null, 401);
             }
-    }
+
+            if (Auth::guard('api')->user()->role != 'owner') {
+                return Helper::ResponseAPI('Error! You do not have permission to perform this action', null, 403);
+            }
+        }
 
         return $next($request);
     }
